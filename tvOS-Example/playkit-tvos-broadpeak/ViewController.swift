749272d5fb2a4755f41c388bf2c57da768a6f7f8
Nilit Danan <nilit.danan@kaltura.com>
2021-01-03 09:40:49 +0200

    tvOS test sample

---
+++ tvOS-Example/playkit-tvos-broadpeak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playkit-tvos-broadpeak
+//
+//  Created by Nilit Danan on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
